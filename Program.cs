97b4884f65ffbe4caa0cc85d58936a951db0af2a
Jurid <juri.d@mail.ru>
2018-10-24 11:49:06 +0300

    простая программа на C#

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Hello, World!");
+
+            // Ожидание нажатия клавиши Enter перед завершением работы
+            Console.ReadLine();
+        }
+    }
+}
